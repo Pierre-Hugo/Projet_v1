a4cb7f0d3b0dacc4595cb9dbd0df86d397e94b2e
Pierre-Hugo <piter.callofduty@gmail.com>
2023-09-11 16:38:05 -0400

    WS updated
    clients authentification is not done and users id need to be added

--- websockets/server/index.js
+++ websockets/server/index.js
@@ -4,8 +4,6 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", (ws, request, client) => {
     console.log("New client connected");
-    client = Math.random().toString(16).slice(2)
-    console.log(wss.client);
 
     ws.on("message", data => {
         console.log(`Client ${client} has sent us: ${data}`);
