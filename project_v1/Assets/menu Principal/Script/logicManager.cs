3f8093394d214c2cb77bcd8d620995260e8559e5
Jean-Fe <104162894+Jean-Fe@users.noreply.github.com>
2023-10-06 10:39:11 -0400

    variable audioMusique lier au slider

--- project_v1/Assets/menu Principal/Script/logicManager.cs
+++ project_v1/Assets/menu Principal/Script/logicManager.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
-public class MainMenuManager : MonoBehaviour
+public class LogicManager : MonoBehaviour
 {
+    public AudioMixer volumeMusique;
 
-    
 
     private void Start()
     {
-        
+
     }
     public void LoadScene(string nameScene)
     {
         SceneManager.LoadScene(nameScene);
     }
 
+    public void askCreateRoom()
+    {
+
+    }
+
+    public void setVolume(float volume)
+    {
+        volumeMusique.SetFloat("Volume", volume);
+    }
+
+
     public void closeGame()
     {
         Application.Quit();
     }
 
-    
+
 }
+
