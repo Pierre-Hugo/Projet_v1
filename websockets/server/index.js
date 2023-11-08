551f59a3e5d8f4cd0adaf61155cf066176c0a086
Pierre-Hugo <piter.callofduty@gmail.com>
2023-11-08 14:57:53 -0500

    Vérification des ID
    Vérification si les ID sont déja utilisé par d'autres clients

--- websockets/server/index.js
+++ websockets/server/index.js
@@ -2,8 +2,10 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on("connection", (ws, request) => {
-    console.log("New client connected");
+const clientIds = new Set();
+
+wss.on("connection", (ws) => {
+    console.log("New client attempting to connect");
 
     ws.isFirstMessage = true;
 
@@ -11,26 +13,53 @@ wss.on("connection", (ws, request) => {
         const messageStr = data.toString();
 
         if (ws.isFirstMessage) {
-            ws.clientId = messageStr;  // Le client envoie son ID au premier message
-            console.log(`Client ${ws.clientId} sent us: ${data}`);
+            const proposedId = messageStr;  // Le client envoie son ID au premier message
+            
+            // Vérifier si l'ID est déjà utilisé
+            if (clientIds.has(proposedId)) {
+                ws.send("ID already in use");
+                ws.close(); // Fermer la connexion si l'ID est déjà pris
+                console.log(`Connection refused, ID ${proposedId} already in use`);
+                return;
+            }
+            
+            // Ajouter le nouvel ID au registre et au WebSocket client
+            clientIds.add(proposedId);
+            ws.clientId = proposedId;
+            ws.send("OK");
+            console.log(`Client ${ws.clientId} connected with unique ID`);
             ws.isFirstMessage = false;
             return;
         }
 
+        // Traitement des messages suivants
         const [targetClientId, messageContent] = messageStr.split(':', 2);
+
+        if (!targetClientId || !messageContent) {
+            return;
+        }
 
         console.log(`${ws.clientId} sends to ${targetClientId}: ${messageContent}`);
 
-        // Trouver le client et envoyer le message (data)
+        // Trouver le client cible et envoyer le message
+        let targetClientFound = false;
         wss.clients.forEach(client => {
             if (client.clientId === targetClientId && client.readyState === WebSocket.OPEN) {
                 client.send(`${ws.clientId}:${messageContent}`);
+                targetClientFound = true;
             }
         });
+
+        if (!targetClientFound) {
+            ws.send(`Client ID ${targetClientId} not found`);
+        }
     });
 
     ws.on("close", () => {
-        console.log("Client disconnected");
+        // Supprimer l'ID du client de l'ensemble lorsqu'il se déconnecte
+        if (ws.clientId) {
+            clientIds.delete(ws.clientId);
+            console.log(`Client ID ${ws.clientId} disconnected`);
+        }
     });
-
 });
